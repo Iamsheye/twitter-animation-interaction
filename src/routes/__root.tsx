97cd8e194fe76e89ce60760d19a7f96165fe1a55
Majekodunmi Oluwaseye <65172089+Iamsheye@users.noreply.github.com>
2024-05-17 17:40:53 +0100

    Initial commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,30 @@
+import React, { Suspense } from "react";
+import {
+  createRootRoute,
+  Outlet,
+  ScrollRestoration,
+} from "@tanstack/react-router";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <ScrollRestoration />
+      <Outlet />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
+    </>
+  ),
+});
